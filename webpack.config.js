58f60082db9ab66f8f365bbd2ad66c300524fd68
KirillShnyak <kshnyak@bk.ru>
2023-08-29 13:06:04 +0500

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const { resolve } = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+const mode = process.env.NODE_ENV;
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true,
+    environment: {
+      arrowFunction: false,
+    },
+  },
+  devtool: mode === "production" ? "source-map" : "eval-source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: resolve(__dirname, "index.html"),
+    }),
+    new HtmlWebpackPlugin({
+      filename: "blog.html",
+      template: "./blog.html",
+    }),
+    new HtmlWebpackPlugin({
+      filename: "feedback.html",
+      template: "./feedback.html",
+    }),
+    new HtmlWebpackPlugin({
+      filename: "about.html",
+      template: "./about.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+
+  optimization: {
+    minimizer: [`...`, new CssMinimizerPlugin()],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[hash][ext]",
+        },
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  devServer: {
+    compress: true,
+    port: 9100,
+  },
+};
